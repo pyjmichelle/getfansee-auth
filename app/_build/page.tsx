144ada11118e5ce10a96347ba8cb416c31a4c240
蒲怡君 <puyijun@puyijundeMacBook-Pro.local>
2025-12-20 16:40:35 +0800

    chore: add build mark page

---
+++ app/_build/page.tsx
@@ -0,0 +1,10 @@
+export default function BuildPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Build Mark</h1>
+      <p>commit: 70a5ada</p>
+      <p>time: 2025-12-20</p>
+    </main>
+  );
+}
+
